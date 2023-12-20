a92b4d6b955a39f6fe5e80d726ad82d1d5feb22b
SophieGrahamUB <143078306+SophieGrahamUB@users.noreply.github.com>
2023-12-20 11:14:42 +0000

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -19,7 +19,7 @@ const about = () => {
         <Center>
           <TextMargin
             body={
-              "Founder of Magic Word Bracelet! For the longest time i've always believed in magic. I think that's why I'm also a clown! Magic can come in so many different forms that it's hard to put a pin in what magic really means. But to me, that's the beauty of it. Magic is subjective! It's what YOU think magic is. That's why I came up with Magic Word Bracelet! It's YOUR magic, YOUR words, YOUR way!"
+              "I'm the founder of Magic Word Bracelet! For the longest time i've always believed in magic. I think that's why I'm also a clown! Magic can come in so many different forms that it's hard to put a pin in what magic really means. But to me, that's the beauty of it. Magic is subjective! It's what YOU think magic is. That's why I came up with Magic Word Bracelet! It's YOUR magic, YOUR words, YOUR way!"
             }
           />
         </Center>
